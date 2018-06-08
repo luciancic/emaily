be8fc2c6fd73a3f01edeadf5e731ba4770778f66
lucian95 <luciancondrea95@gmail.com>
2018-06-08 10:16:03 -0400

    Connect to db through mongoose

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+const config = require('config');
 const express = require('express');
+const mongoose = require('mongoose');
 require('./services/passport');
+
+const MONGODB_URI = process.env.MONGODB_URI || config.get('MONGODB_URI');
+mongoose.connect(MONGODB_URI);
+
 const app = express();
 
 require('./routes/auth')(app);
