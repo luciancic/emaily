6fa4fd8975553e37a407a7d99d6050bc04db9a15
lucian95 <luciancondrea95@gmail.com>
2018-06-08 14:43:40 -0400

    Add cookie session and init passport session

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const config = require('config');
+const cookieSession = require('cookie-session');
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
 require('./models');
 require('./services/passport');
+const app = express();
 
 const MONGODB_URI = process.env.MONGODB_URI || config.get('MONGODB_URI');
 mongoose.connect(MONGODB_URI);
 
-const app = express();
+const COOKIE_KEY = process.env.COOKIE_KEY || config.get('COOKIE_KEY');
+app.use(cookieSession({
+  maxAge: 30 * 24 * 60 * 60 * 1000,
+  keys: [COOKIE_KEY]
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/auth')(app);
 
