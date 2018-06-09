a3c67b4703a19b7d9e1be74e7be3e3086f70584e
lucian95 <luciancondrea95@gmail.com>
2018-06-09 15:00:45 -0400

    Style header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-export default () => {
-  return (
-    <p>This is a header placeholder.</p>
-  )
+class Header extends Component {
+  render() {
+    return (     
+      <nav>
+        <div className="nav-wrapper blue lighten-2">
+          <Link to="/" class="brand-logo" style={{marginLeft: "20px"}} >Emaily</Link>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li><Link to="/auth/google">Sign in with google</Link></li>
+          </ul>
+        </div>
+      </nav>
+    )
+  }
 }
+
+export default Header;
