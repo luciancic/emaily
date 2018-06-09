1dc24abb7c41e3c8293ca408ba208dcdfcd772de
lucian95 <luciancondrea95@gmail.com>
2018-06-09 11:08:20 -0400

    Add store provider as App parent

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(() => [], {}, applyMiddleware());
+
+ReactDOM.render(
+  <Provider store={store}><App /></ Provider>, 
+  document.getElementById('root')
+);
 registerServiceWorker();
