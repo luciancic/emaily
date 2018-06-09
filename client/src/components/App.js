b604cd46abc20656af60f76967ac1b7def2d28a3
lucian95 <luciancondrea95@gmail.com>
2018-06-09 14:11:06 -0400

    Add basic routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './Header';
+import Dashboard from './Dashboard';
+import Landing from './Landing';
 
-
-const App = () => {
-  return (
-    <h1>Welcome to Emaily!</h1>
-  )
-}
+const App = () => 
+    <Router>
+      <div id="app">
+        <Header />
+        
+        <Route exact path="/" component={Landing} />
+        <Route path="/dashboard" component={Dashboard} />
+      </div>
+    </Router>
 
 export default App;
