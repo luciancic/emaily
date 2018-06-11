f01a512b4a9e66535f0ce0c957e9ef2307e0cb66
lucian95 <luciancondrea95@gmail.com>
2018-06-11 10:34:14 -0400

    Add action creator for fetching current user

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
 import Header from './Header';
 import Dashboard from './Dashboard';
 import Landing from './Landing';
 
 import 'materialize-css/dist/css/materialize.min.css';
 
-const App = () => 
-    <Router>
-      <div id="app">
-        <Header />
-        
-        <Route exact path="/" component={Landing} />
-        <Route path="/dashboard" component={Dashboard} />
-      </div>
-    </Router>
+class App extends Component { 
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+  
+  render(){
+    return (
+      <Router>
+        <div id="app">
+          <Header />
+          
+          <Route exact path="/" component={Landing} />
+          <Route path="/dashboard" component={Dashboard} />
+        </div>
+      </Router>
+    )
+  }
 
-export default App;
+}
+export default connect(null, actions)(App);
