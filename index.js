1a4534197d7aedc392e87a5a313f896eb510097a
lucian95 <luciancondrea95@gmail.com>
2018-06-11 11:18:14 -0400

    Configured to serve client on heroku

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cookieSession = require('cookie-session');
 const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
+const path = require('path');
 require('./models');
 require('./services/passport');
 const app = express();
@@ -11,12 +12,16 @@ const MONGODB_URI = process.env.MONGODB_URI || config.get('MONGODB_URI');
 mongoose.connect(MONGODB_URI);
 
 const COOKIE_KEY = process.env.COOKIE_KEY || config.get('COOKIE_KEY');
+
+
 app.use(cookieSession({
   maxAge: 30 * 24 * 60 * 60 * 1000,
   keys: [COOKIE_KEY]
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.use(express.static(path.resolve(__dirname, 'client/build')));
 
 require('./routes/auth')(app);
 
