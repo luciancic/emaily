a0ecc81312b3361cc47e2520d31de66148ceecab
lucian95 <luciancondrea95@gmail.com>
2018-06-11 11:01:40 -0400

    Add basic marketing content to landing page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 export default () => {
   return (
-    <h2>Landing</h2>
+    <div className="center-align">
+      <h1>Emaily</h1>
+      <p>The easiest way to collect user feedback!</p>
+    </div>
   )
 }
