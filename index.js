90e997d5ab4e17ff344058defee3b9781d9f00fb
lucian95 <luciancondrea95@gmail.com>
2018-06-11 11:48:14 -0400

    Correct routes to static files

--- index.js
+++ index.js
@@ -21,9 +21,15 @@ app.use(cookieSession({
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(express.static(path.resolve(__dirname, 'client/build')));
-
 require('./routes/auth')(app);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
