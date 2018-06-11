c79cc37829e0ee81ddbb1656ef6f8747795184fd
lucian95 <luciancondrea95@gmail.com>
2018-06-11 14:31:13 -0400

    Remove unnecessary cors trick (bug in own browser)

--- index.js
+++ index.js
@@ -13,20 +13,12 @@ mongoose.connect(MONGODB_URI);
 
 const COOKIE_KEY = process.env.COOKIE_KEY || config.get('COOKIE_KEY');
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 app.use(cookieSession({
   maxAge: 30 * 24 * 60 * 60 * 1000,
   keys: [COOKIE_KEY]
 }));
 app.use(passport.initialize());
 app.use(passport.session());
-
-// Circumvent CORS issue
 
 require('./routes/auth')(app);
 
