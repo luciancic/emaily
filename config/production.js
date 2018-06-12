6c44d7ff82aacb538e742ea93c7267db6d4ee9e3
lucian95 <luciancondrea95@gmail.com>
2018-06-12 10:08:20 -0400

    Add stripe api key references

--- config/production.js
+++ config/production.js
@@ -3,6 +3,10 @@ module.exports = {
     CLIENT_ID: process.env.GOOGLE_OAUTH_CLIENT_ID,
     CLIENT_SECRET: process.env.GOOGLE_OAUTH_CLIENT_SECRET
   },
+  STRIPE: {
+    PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY,
+    SECRET_KEY: process.env.STRIPE_SECRET_KEY
+  },
   MONGODB_URI: process.env.MONGODB_URI,
   COOKIE_KEY: process.env.COOKIE_KEY
 };
