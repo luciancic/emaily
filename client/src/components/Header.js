4debd10ad88ae9ae0a91062bdd1b43022b469117
lucian95 <luciancondrea95@gmail.com>
2018-06-12 12:27:09 -0400

    Add credit counter to header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -11,7 +11,8 @@ class Header extends Component {
       default: {
         return [
           <li key="1"><Payment /></li>,
-          <li key="2"><a href="/api/logout">Logout</a></li>
+          <li key="2" style={{ margin: "0 20px"}}>Credits: {this.props.auth.credits}</li>,
+          <li key="3"><a href="/api/logout">Logout</a></li>
         ]
       }
     }
