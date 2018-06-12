78896f8228fca48d07473962b36543e7af809773
lucian95 <luciancondrea95@gmail.com>
2018-06-12 11:11:47 -0400

    Send post request from client with stripe token

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,5 +3,10 @@ import { FETCH_USER } from './types';
 
 export const fetchUser = () => async (dispatch) => {
   const res = await axios.get('/api/current_user');
-  dispatch({ type: FETCH_USER, payload: res.data});
+  dispatch({ type: FETCH_USER, payload: res.data });
+}
+
+export const handlePaymentToken = (token) => async (dispatch) => {
+  const res = await axios.post('/api/payment', token);
+  dispatch({ type: FETCH_USER, payload: res.data });
 }
--- client/src/components/Payment.js
+++ client/src/components/Payment.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 import StripeCheckout from 'react-stripe-checkout';
 
 class Payment extends Component {
@@ -9,9 +11,9 @@ class Payment extends Component {
       description="Add 5 credits to your account"
       name="Emaily Credits"
       stripeKey={process.env.REACT_APP_STRIPE_KEY}
-      token={token => console.log(token)}
+      token={token => this.props.handlePaymentToken(token)}
     ><button className="btn orange darken-1">Add credits</button></StripeCheckout>
   }
 }
 
-export default Payment;
+export default connect(null, actions)(Payment);
