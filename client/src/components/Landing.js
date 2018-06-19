38154434c23f5a51e557d6ae4e3b54e699c1fadd
luciancic <luciancondrea95@gmail.com>
2018-06-19 14:07:59 -0400

    Redirect from landing to dashboard if logged in

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-export default () => {
-  return (
-    <div className="center-align">
-      <h1>Emaily</h1>
-      <p>The easiest way to collect user feedback!</p>
-    </div>
-  )
+class Landing extends Component {
+  render() {
+    return (
+      <div className="center-align">
+        <h1>Emaily</h1>
+        <p>The easiest way to collect user feedback!</p>
+        { this.props.auth ? <Redirect to="/dashboard" /> : null }
+      </div>
+    )
+  }
 }
+
+function mapStateToProps({ auth }) {
+  return { auth }
+}
+
+export default connect(mapStateToProps)(Landing);
