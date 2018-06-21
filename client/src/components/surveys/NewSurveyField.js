79c83730f49d652f509a16570d0a8c9661997935
luciancic <luciancondrea95@gmail.com>
2018-06-21 11:59:40 -0400

    Replace survey form placeholder by label

--- client/src/components/surveys/NewSurveyField.js
+++ client/src/components/surveys/NewSurveyField.js
@@ -3,7 +3,8 @@ import React from 'react';
 export default ({ input, label, meta: { error, touched } }) => {
     return (
         <div style={{ marginBottom: '15px'}}>
-            <input {...input} id={input.name} placeholder={label} style={{ marginBottom: '8px'}}/>
+            <label>{label}</label>
+            <input {...input} id={input.name} style={{ marginBottom: '8px'}}/>
             <span className="red-text">{ touched && error }</span>
         </div>
     )
